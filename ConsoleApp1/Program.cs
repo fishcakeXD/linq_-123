a810933040c0e468437227c724aab35d5be2a2c6
a0918959733@gmail.com <a0918959733@gmail.com>
2023-02-21 13:05:58 +0800

    加入專案檔案。

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = Console.ReadLine().Split(',');
+            int[] num = Array.ConvertAll(number,s => int.Parse(s));
+            List<int> list = num.ToList();
+            var num_1 =
+                from p in list
+                where p % 2 != 0
+                select p;
+            var num_2 =
+                from p in list
+                where p % 2 == 0
+                select p;
+            Console.Write("奇數為: ");
+            foreach (var item in num_1)
+            {
+                Console.Write($"{item},");
+            }
+            Console.WriteLine();
+            Console.Write("偶數為: ");
+            foreach(var item in num_2)
+            {
+              Console.Write($"{item},");
+            }
+            Console.WriteLine();
+            Console.ReadLine();
+        }
+    }
+}
---
+++ linq_練習123/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.Remoting.Messaging;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace linq_練習123
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] number = Console.ReadLine().Split(',');
+            List<string> list = number.ToList();
+            list.Reverse();
+            
+            foreach(var item in list)
+            {               
+                Console.Write(item+",");   
+            }
+            Console.ReadLine();
+
+
+        }
+    }
+}
